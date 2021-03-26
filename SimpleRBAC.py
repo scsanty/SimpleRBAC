ec305cab503b5ee7c37d4793eebcd833d95ed389
scsanty <64268196+scsanty@users.noreply.github.com>
2021-03-26 14:06:55 +0530

    Update SimpleRBAC.py

--- SimpleRBAC.py
+++ SimpleRBAC.py
@@ -50,12 +50,12 @@ class SimpleRBAC():
 	def if_exists_in_master(self, type, type_name):
 		type = type.upper()
 		return self.conn.execute(f"select (case when count(*) > 0 then True else False end) from {type}_MASTER" \
-			" WHERE {type}_NAME = '" + type_name + "';")
+			f" WHERE {type}_NAME = '" + type_name + "';").fetchone()[0]
 
 	def add_in_masters(self, type, type_name, description):
 		type = type.upper()
 		try:
-			self.conn.execute(f"insert into {type}_MASTER ({type}_NAME, DESCRIPTION) values ('{type_name}', '{description}');")
+			self.conn.execute(f"INSERT INTO {type}_MASTER ({type}_NAME, DESCRIPTION) VALUES ('{type_name}', '{description}');")
 			self.conn.commit()
 			return f'{type} Created Successfully'
 		except sqlite3.IntegrityError as e:
@@ -69,11 +69,11 @@ class SimpleRBAC():
 			if not self.if_exists_in_master(type, type_name):
 				return f"{type} Name doesn't exists"
 			if description:
-				self.conn.execute(f"update {type}_MASTER set {type}_NAME = '{type_name}', DESCRIPTION = '{description}';")
+				self.conn.execute(f"UPDATE {type}_MASTER SET {type}_NAME = '{type_name}', DESCRIPTION = '{description}';")
 			else:
-				self.conn.execute(f"update {type}_MASTER set {type}_NAME = '{type_name}';")
+				self.conn.execute(f"UPDATE {type}_MASTER SET {type}_NAME = '{type_name}';")
 			self.conn.commit()
-			return f'{type} Name Updated Successfully'
+			return f'{type} Name updated Successfully'
 		except sqlite3.IntegrityError as e:
 			return f'{type} Name already exists'
 		except Exception as e:
@@ -84,13 +84,17 @@ class SimpleRBAC():
 		try:
 			if not self.if_exists_in_master(type, type_name):
 				return f"{type} Name doesn't exists"
-			self.conn.execute(f"delete {type}_MASTER where {type}_NAME = '{type_name}';")
+			self.conn.execute(f"DELETE FROM {type}_MASTER WHERE {type}_NAME = '{type_name}';")
 			self.conn.commit()
 			return 'Role Name Updated Successfully'
 		except sqlite3.IntegrityError as e:
 			return 'Role Name already exists'
 		except Exception as e:
 			return str(e)
+
+	def get_from_masters(self, type):
+		elements = self.conn.execute("select {type}_NAME, DESCRIPTION from {type}_MASTER").fetchall()
+		return elements
 
 	def resolve_pk(self, type, type_name):
 		type = type.upper()
@@ -104,9 +108,9 @@ class SimpleRBAC():
 
 	def add_user_role_map(self, user_id, role_id, resolve_pk=False):
 		role_id = role_id if isinstance(role_id, list) else [role_id]
-		if resolve:
-			user_id = resolve_pk('USER', user_id)
-			role_id = [resolve_pk(i) for i in role_id]
+		if resolve_pk:
+			user_id = self.resolve_pk('USER', user_id)
+			role_id = [self.resolve_pk('ROLE', i) for i in role_id]
 		try:
 			query = "INSERT INTO USER_ROLE_MAP (USER_ID, ROLE_ID) VALUES "
 			query += ", ".join([f"({user_id}, {id})" for id in role_id])
@@ -116,27 +120,185 @@ class SimpleRBAC():
 		except Exception as e:
 			return str(e)
 
-	def get_available_roles(self):
-		elements = self.conn.execute("select ROLE_NAME, DESCRIPTION from ROLE_MASTER").fetchall()
-		return elements#pk, role_table
+	def delete_user_role_map(self, user_id, role_id=None, resolve_pk=False):
+		if user_id and role_id:
+			role_id = role_id if isinstance(role_id, list) else [role_id]
+			if resolve_pk:
+				user_id = self.resolve_pk('USER', user_id)
+				role_id = [self.resolve_pk('ROLE', i) for i in role_id]
+			try:
+				query = "DELETE FROM USER_ROLE_MAP WHERE (USER_ID ||','|| ROLE_ID) in "
+				query += str(tuple(f"{user_id},{id}" for id in role_id))
+				self.conn.execute(query)
+				self.conn.commit()
+			except Exception as e:
+				return str(e)
+		if not user_id and role_id:
+			try:
+				role_id = self.resolve_pk('ROLE', role_id) if resolve_pk == True else role_id
+				query = "DELETE FROM USER_ROLE_MAP WHERE ROLE_ID = " + role_id
+				self.conn.execute(query)
+				self.conn.commit()
+			except Exception as e:
+				return str(e)
+		else:
+			try:
+				user_id = self.resolve_pk('USER', user_id) if resolve_pk == True else user_id
+				query = "DELETE FROM USER_ROLE_MAP WHERE USER_ID = " + user_id
+				self.conn.execute(query)
+				self.conn.commit()
+			except Exception as e:
+				return str(e)
 
-	def add_role_resource_auth_map(self, role_name, resource_name, auth_name):
-		pass
+	def get_roles_assigned_to_user(self, uname):
+		query = "SELECT " \
+			"c.ROLE_NAME, " \
+			"c.DESCRIPTION " \
+			"FROM USER_MASTER a " \
+			"LEFT JOIN USER_ROLE_MAP b ON a.ID = b.USER_ID " \
+			"LEFT JOIN ROLE_MASTER c ON c.ID = b.ROLE_ID " \
+			"WHERE a.uname = '" + uname + "';"
+		elements = self.conn.execute(query).fetchall()
+		return elements
 
-	def get_available_resources(self):
-		elements = self.conn.execute("select * from RESOURCE_MASTER").fetchall()
-		role_table = [[i+1, elements[i][1], elements[i][2]] for i in range(len(elements))]
-		primary_keys = [elements[i][0] for i in range(len(elements))]
-		return primary_keys, role_table
+	def get_auth_for_resource(self, resource_name):
+		query = "SELECT " \
+			"c.AUTH_NAME, " \
+			"c.DESCRIPTION " \
+			"FROM RESOURCE_MASTER a " \
+			"LEFT JOIN RESOURCE_AUTH_MAP b ON b.RESOURCE_ID = a.ID " \
+			"LEFT JOIN AUTH_MASTER c ON c.ID = b.AUTH_ID " \
+			"WHERE a.RESOURCE_NAME = '" +resource_name+ "';"
 
-	def get_available_auth_for_resource(self, resource_name):
-		pass
+		elements = self.conn.execute(query).fetchall()
+		return elements
+
+	def add_role_resource_auth_map(self, role_id, resource_id, auth_id, resolve_pk=False):
+		auth_id = auth_id if isinstance(auth_id, list) else [auth_id]
+		if resolve_pk:
+			role_id = self.resolve_pk('ROLE', role_id)
+			resource_id = self.resolve_pk('RESOURCE', resource_id)
+			auth_id = [self.resolve_pk('ROLE', i) for i in auth_id]
+		try:
+			query = "INSERT INTO ROLE_RESOURCE_AUTH_MAP (ROLE_ID, RESOURCE_ID, AUTH_ID) VALUES "
+			query += ", ".join([f"({role_id}, {resource_id}, {id})" for id in auth_id])
+			query += " ON CONFLICT DO NOTHING;"
+			self.conn.execute(query)
+			self.conn.commit()
+		except Exception as e:
+			return str(e)
+
+	def delete_role_resource_auth_map(self, role_id=None, resource_id=None, auth_id=None, pk_id=None, resolve_pk=False):
+		if pk_id:
+			pk_id = pk_id if isinstance(pk_id, list) else [pk_id]
+			try:
+				query = "DELETE FROM ROLE_RESOURCE_AUTH_MAP WHERE ID in "
+				query += str(tuple(pk_id))
+				self.conn.execute(query)
+				self.conn.commit()
+			except Exception as e:
+				return str(e)
+
+		elif role_id and resource_id and auth_id:
+			auth_id = auth_id if isinstance(auth_id, list) else [auth_id]
+			if resolve_pk:
+				role_id = self.resolve_pk('ROLE', role_id)
+				resource_id = self.resolve_pk('RESOURCE', resource_id)
+				auth_id = [self.resolve_pk('AUTH', i) for i in auth_id]
+			try:
+				query = "DELETE FROM ROLE_RESOURCE_AUTH_MAP WHERE (ROLE_ID ||','|| RESOURCE_ID ||','|| AUTH_ID) in "
+				query += str(tuple(f"{role_id},{resource_id},{id}" for id in auth_id))
+				self.conn.execute(query)
+				self.conn.commit()
+			except Exception as e:
+				return str(e)
+
+		elif role_id and resource_id:
+			resource_id = resource_id if isinstance(resource_id, list) else [resource_id]
+			if resolve_pk:
+				role_id = self.resolve_pk('ROLE', resource_id)
+				resource_id = [self.resolve_pk('RESOURCE', i) for i in resource_id]
+			try:
+				query = "DELETE FROM ROLE_RESOURCE_AUTH_MAP WHERE (ROLE_ID ||','|| RESOURCE_ID) in "
+				query += str(tuple(f"{role_id},{id}" for id in resource_id))
+				self.conn.execute(query)
+				self.conn.commit()
+			except Exception as e:
+				return str(e)
+
+		elif role_id:
+			if resolve_pk:
+				role_id = self.resolve_pk('ROLE', role_id)
+			try:
+				query = f"DELETE FROM ROLE_RESOURCE_AUTH_MAP WHERE ROLE_ID = {role_id}"
+				self.conn.execute(query)
+				self.conn.commit()
+			except Exception as e:
+				return str(e)
+
+		elif not role_id and resource_id:
+			try:
+				resource_id = self.resolve_pk('RESOURCE', resource_id) if resolve_pk == True else resource_id
+				query = "DELETE FROM RESOURCE_AUTH_MAP WHERE RESOURCE_ID = " + resource_id
+				self.conn.execute(query)
+				self.conn.commit()
+			except Exception as e:
+				return str(e)
+
+	def get_role_resource_auth_map(self):
+		query = "SELECT " \
+			"a.ID, " \
+			"b.ROLE_NAME, " \
+			"c.RESOURCE_NAME, " \
+			"d.AUTH_NAME " \
+			"FROM ROLE_RESOURCE_AUTH_MAP a " \
+			"LEFT JOIN ROLE_MASTER b ON b.ID = a.ROLE_ID " \
+			"LEFT JOIN RESOURCE_MASTER c ON c.ID = a.RESOURCE_ID " \
+			"LEFT JOIN AUTH_MASTER d ON d.ID = a.AUTH_ID"
+
+		elements = self.conn.execute(query).fetchall()
+		return elements
 
 
+	def add_resource_auth_map(self, resource_id, auth_id, resolve_pk=False):
+		auth_id = auth_id if isinstance(auth_id, list) else [auth_id]
+		if resolve:
+			resource_id = self.resolve_pk('RESOURCE', resource_id)
+			auth_id = [self.resolve_pk('AUTH', i) for i in auth_id]
+		try:
+			query = "INSERT INTO RESOURCE_AUTH_MAP (RESOURCE_ID, AUTH_ID) VALUES "
+			query += ", ".join([f"({resource_id}, {id})" for id in auth_id])
+			query += " ON CONFLICT DO NOTHING;"
+			self.conn.execute(query)
+			self.conn.commit()
+		except Exception as e:
+			return str(e)
 
-	def add_resource_auth_map(self, resource_name, auth_name, resolve_pk=False):
+	def delete_resource_auth_map(self, resource_id, auth_id=None, resolve_pk=False):
+		if auth_id:
+			auth_id = auth_id if isinstance(auth_id, list) else [auth_id]
+			if resolve_pk:
+				resource_id = self.resolve_pk('RESOURCE', resource_id)
+				auth_id = [self.resolve_pk('AUTH', i) for i in auth_id]
+			try:
+				query = "DELETE FROM RESOURCE_AUTH_MAP WHERE (RESOURCE_ID ||','|| AUTH_ID) in "
+				query += str(tuple(f"{resource_id},{id}" for id in auth_id))
+				self.conn.execute(query)
+				self.conn.commit()
+			except Exception as e:
+				return str(e)
+		else:
+			try:
+				resource_id = self.resolve_pk('RESOURCE', resource_id) if resolve_pk == True else resource_id
+				query = "DELETE FROM RESOURCE_AUTH_MAP WHERE RESOURCE_ID = " + resource_id
+				self.conn.execute(query)
+				self.conn.commit()
+			except Exception as e:
+				return str(e)
 
-		pass
-
-	def validate_access(self, uname, auth_name):
-		pass
+	def if_admin(self, uname):
+		if self.if_exists_in_master('USER', uname):
+			uid = self.resolve_pk('USER', uname)
+			return self.conn(f"select (case when count(*) > 0 then True else False end) " \
+				"from USER_ROLE_MAP " \
+				"WHERE USER_ID = " + str(uid) + " AND ROLE_ID = 0;").fetchone()[0]
